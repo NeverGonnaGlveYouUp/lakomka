0e98309d60fc6ff6b75d785da1904860436c8c0f
SuvorovJA <riskiroy@yandex.ru>
2025-10-10 20:17:09 +0700

    замена RSQL библиотеки на rsql-jpa-spring-boot-starter

--- api/src/main/java/com/lakomka/controller/ProductController.java
+++ api/src/main/java/com/lakomka/controller/ProductController.java
@@ -2,24 +2,18 @@ package com.lakomka.controller;
 
 import com.lakomka.models.product.Product;
 import com.lakomka.repository.product.ProductFilterRepository;
-import com.lakomka.specifications.CustomRsqlVisitor;
-import cz.jirutka.rsql.parser.RSQLParser;
-import cz.jirutka.rsql.parser.ast.Node;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static io.github.perplexhub.rsql.RSQLJPASupport.toSort;
+import static io.github.perplexhub.rsql.RSQLJPASupport.toSpecification;
 
 @Controller
 public class ProductController {
@@ -33,25 +27,11 @@ public class ProductController {
             @RequestParam(value = "search") String search,
             @RequestParam(value = "page") Integer page,
             @RequestParam(value = "size") Integer size,
-            @RequestParam(value = "sort") String sort) {;
-        Node rootNode = new RSQLParser().parse(search);
-        Specification<Product> spec = rootNode.accept(new CustomRsqlVisitor<>());
-        Pattern pattern = Pattern.compile("(\\w+);\\s*(asc|desc)");
-        Matcher matcher = pattern.matcher(sort);
-        Page<Product> products;
-        if (matcher.find()) {
-            String firstGroup = matcher.group(1);
-            String secondGroup = matcher.group(2);
-            if (Objects.equals(secondGroup, "asc")) {
-                products = productRepository.findAll(spec, PageRequest.of(page, size, Sort.by(firstGroup).ascending()));
-            } else if (Objects.equals(secondGroup, "desc")) {
-                products = productRepository.findAll(spec, PageRequest.of(page, size, Sort.by(firstGroup).descending()));
-            } else {
-                products = productRepository.findAll(spec, PageRequest.of(page, size));
-            }
-        } else {
-            products = productRepository.findAll(spec, PageRequest.of(page, size));
-        }
-        return products;
+            @RequestParam(value = "sort") String sort) {
+
+        Specification<Product> searchSpecification = toSpecification(search);
+        Specification<Product> searchSpecificationSorted = searchSpecification.and(toSort(sort));
+        return productRepository.findAll(searchSpecificationSorted, PageRequest.of(page, size));
+
     }
 }

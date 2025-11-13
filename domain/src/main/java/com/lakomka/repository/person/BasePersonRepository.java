c1e50a53033568a2880dac98dd80fdd8e8bad56c
SuvorovJA <riskiroy@yandex.ru>
2025-11-13 20:12:39 +0700

    удаление spring-data-rest

--- domain/src/main/java/com/lakomka/repository/person/BasePersonRepository.java
+++ domain/src/main/java/com/lakomka/repository/person/BasePersonRepository.java
@@ -2,11 +2,9 @@ package com.lakomka.repository.person;
 
 import com.lakomka.models.person.BasePerson;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
-@RestResource(exported = false)
 public interface BasePersonRepository extends JpaRepository<BasePerson, Long> {
     Optional<BasePerson> findByLogin(String login);
 }
